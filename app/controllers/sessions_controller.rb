d34de8c17bea36e01cc588c85e2f004b20612d48
yuki <yukkixsu8@yahoo.co.jp>
2017-10-13 16:11:44 +0900

    サインイン,サインアウト編集

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,13 @@ class SessionsController < ApplicationController
       sign_in user
       redirect_to user
     else
-      flash[:error] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
   def destroy
     sign_out
-    redirect_to root_url 
+    redirect_to root_url
   end
 end
