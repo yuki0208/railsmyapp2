7023b4bdd6c1ff4879b100414ddae6b99d94c72c
yuki <yukkixsu8@yahoo.co.jp>
2017-11-01 12:01:55 +0900

    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,18 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
-  resources :posts
   resources :users do
     member do
       get :following, :followers
     end
   end
+  resources :posts, only: [:create, :destroy]
   resources :posts do
     resources :comments, only: [:create, :destroy]
 	end
   resources :sessions, only: [:new, :create, :destroy]
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :relationships, only: [:create, :destroy]
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
